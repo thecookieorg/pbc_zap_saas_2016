25b8ebd26703494a0780db50272342b5477000d2
Marko Manojlovic <marko@thecookie.org>
2016-04-09 15:43:43 -0700

    added views for devise_invitable

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :role)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :role)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,26 @@
 class User < ActiveRecord::Base
+	# attr_accessor :invitation_instructions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  	devise :invitable, :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable
+
+	# def self.invite_partner!(attributes={}, invited_by=nil)
+	# 	self.invite!(attributes, invited_by) do |invitable|
+	#		invitable.invitation_instructions = :parther_invitation_instructions
+	#	end
+	# end
+
+	#def self.invite_friend!(attributes={}, invited_by=nil)
+	 #self.invite!(attributes, invited_by) do |invitable|
+	 #  invitable.invitation_instructions = :friend_invitation_instructions
+	 #end
+	#end
 
   #ROLES = %i[admin partner manager senior_accountant junior_accountant owner head_of_accounting accounting_staff]
   #def role_symbols
   #	[role.to_sym]
   #end
 
-  has_many :companies
+  	has_many :companies
 end
